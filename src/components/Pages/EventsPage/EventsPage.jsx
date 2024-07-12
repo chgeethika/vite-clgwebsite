2f625b7eb2f9bed6aeac1bd796829eddbeeebac2
chgeethika <ch.geethika2003@gmail.com>
2024-07-12 11:49:03 +0530

    first commit

---
+++ src/components/Pages/EventsPage/EventsPage.jsx
@@ -0,0 +1,20 @@
+import "./EventsPage.css";
+export default function EventsPage(){
+    return(
+        <> 
+        <div className="events">
+       
+        <h1>Events</h1>
+          <h2>ScienceFair Event</h2>
+          <img src="./science fair.jpg"></img>
+
+          <p>A science fair or engineering fair is an event hosted by a school that offers students the opportunity to experience the practices of science and engineering for themselves.</p>
+      
+          <h2>Sports day</h2>
+          <img src="./sports day.jpg"></img>
+          <p>A sports day in a college is a day dedicated to sports and physical activities</p>
+       
+          </div>
+        </>
+    )
+}
